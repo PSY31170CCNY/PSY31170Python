50dc5990926d2dba3866a84f0c681fb21c291c9a
davebritton <dave@davebritton.com>
2019-01-11 16:21:07 -0500

    added function getpersondata()

--- Personclass.py
+++ Personclass.py
@@ -10,7 +10,7 @@ class Person:
     def hello(self):
         print("Hi there! My name is ",self.name)
 
-
+# test the class:
 Sally = Person('Sally','1 Any Way','123-4567','sally@gmail')
 
 print(Sally.name)
@@ -22,21 +22,29 @@ Bob = Person("Robert",'1234 Drive',email='bob@gmail')
 Bob.hello()
 
 #make a list of the People objects:
+#  -- fills in for the "read the data from storage"
 people=[Bob,Sally]
 drive='/media/dave/PSY31170/'
-###write the name and emails of the people to a csv file for Excel:
-##e=open(drive+'emaillist.csv','w') #open the file
-##for member in people:
-##    # construct the text line for each member:
-##    # put the attributes in quotes separated by a comma,
-##    # like this: "Dave","dave@gmail"
-##    # add a newline at the end so each entry is on a new line
-##    l='"' + member.name + '"' + ',"' +member.email+ '"\n'
-##    e.writelines(l)
-##e.close()
+
+#write the name and emails of the people to a csv file for Excel:
+e=open(drive+'emaillist.csv','w') #open the file
+
+# save the test entries in the file:
+for member in people:
+    # construct the text line for each member:
+    # put the attributes in quotes separated by a comma,
+    # like this: "Dave","dave@gmail"
+    # add a newline at the end so each entry is on a new line
+    l='"' + member.name + '"' + ',"' +member.email+ '"\n'
+    e.writelines(l)
+e.close()
 
 # now read the file and see what it looks like:
 e=open(drive+'emaillist.csv','r')
+# python files (and file-like objects) can be accessed
+# after they are "opened" by referencing the "file handle"
+# which you normally save in a variable
+
 print("......\nread one line at a time and print it:")
 print("each entry has a newline at the end, and the print function adds another one.")
 for l in e: #e is a file object, so this reads the file one line at a time
@@ -83,11 +91,13 @@ for p in peopledict.keys():
 print('===\n')
 n=''
 while n == '':
-    n=input("Go ahead, enter a name to look up their email:")
+    n=input("Go ahead, enter a name to look up their data (or q to quit):")
+    if n in 'Qq'
+        break
     try: # the try-except block catches errors without crashing the program
         print(peopledict['"'+n+'"']) # add the quotes present for csv format
     except:
-        print("Error: ",n,"not found in peopledict. Try again.")
+        print("Error: ",n,"not found in peopledict. Try again or q to quit.")
         n=''
         
 # === TO DO: ===
@@ -101,46 +111,43 @@ while n == '':
 
 
 """
-
-# loop to ask user for name,email,addr,phone
-    fields=['name','email','addr','phone]
-    data={} #use a dictionary to store the new data
+def getpersondata(data={'name':'','email':'','addr':'','phone:''}):
+    # loop to ask user for name,email,addr,phone
+    # is there data already present? if so, save it
+     fields=['name','email','addr','phone]
+     #use a dictionary to store the new data
     for field in fields:
-        data[field]=input("enter the next "+field)
+        if len(data[field]>0):
+             x=input("change "+field+ " from "+data[field]+"?(Y/N)")
+             if x not in 'Yy':
+                 continue
+        data[field]=input("enter the person's "+field  )
+    return data
+             
     # now data[fieldname] returns the value that was entered
 
 # ask user for Person to look up in dictionary of persons:
-    persons = readthepeoplefile(filename) # need to write this function
-        #to define the dictionary of persons
-    while True: # do loop until we hit a break command
-        pname = input('enter the name of the person to find:')
-        pdata = persons[pname] # what if the name is not found? then
-            #python "throws" an Exception  - error message, so use try-except:
-        try:
-            pdata = persons[pname]
-        except: # if pname not found catch the error here
-            # not found should we add it?
-            addnew = input(pname+' not found. Add as new (Y/N)?')
-            if addnew in 'Yy' : #add new person
-                persons[pname] = [pname] # first field is name (the key)
-            else:
-                continue # go to begining of while loop
-        # if here, we need to show the data for this person, and ask if the
-        # user wants to change anything
-    def editfields (pname,persons,fields):
-        print('Here is the current data for ',pname)
-        fieldnum = 0 # position of field in the fieldlist
-        for field in fields:
-            fieldnum +=1
-            print(fieldnum,'. 'field, persons.field)
-        editfield=input("Enter the field number to change (0 to end).")
-        efield=int(editfield)
-        if efield >0 and efield < len(fields)+1:
-            print('current value of',fields[efield], 'is ',\
-                  persons[pname][fields[efield]])
-            newdat=input('Enter new data or leave empty to leave unchanged')
-            if len(newdat) > 0:
-                 persons[pname][fields[efield]]) = newdat
+persons = readthepeoplefile(filename) # need to write this function
+    #to define the dictionary of persons
+while True: # do loop until we hit a break command
+pname = input('enter the name of the person to find:')
+pdata = persons[pname] # what if the name is not found? then
+        #python "throws" an Exception  - error message, so use try-except:
+try:
+        pdata = persons[pname]
+except: # if pname not found catch the error here
+        # not found should we add it?
+        addnew = input(pname+' not found. Add as new (Y/N)?')
+        if addnew in 'Yy' : #add new person
+            persons[pname] = [pname] # first field is name (the key)
+        else:
+            continue # go to begining of while loop
+    # if here, we need to show the data for this person, and ask if the
+    # user wants to change anything
+# let's use the function to edit the fields
+#def editfields (pname,persons,fields):
+data=getpersondata(pdata)
+
             
             
     
