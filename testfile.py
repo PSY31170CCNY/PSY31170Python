157cc70a45c6d51193fc2e49d2461a626472454b
davebritton <dave@davebritton.com>
2019-01-23 14:16:59 -0500

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -1,3 +1,3 @@
 print('test file')
 # the post-class survey URL:
-https://docs.google.com/forms/d/e/1FAIpQLSf9tZghOGV2D44KU2WEq6Fs_EbmRG3IEhemPRRh5Xqved9qQw/closedform
+'https://goo.gl/forms/FpJ2cqs9fhxXQa343'
